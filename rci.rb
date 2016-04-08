50c8187f71904997585829bbb07855121cf07dbd
Sebastian Wrede <sebwrede@googlemail.com>
2016-04-08 23:45:08 +0200

    disabled tests

--- rci.rb
+++ rci.rb
@@ -17,8 +17,9 @@ class Rci < Formula
     # ENV.x11 # if your formula requires any X11 headers
     # ENV.j1  # if your formula's build system can't parallelize
     ENV.universal_binary if build.universal?    
+    args = std_cmake_args + %W[-DBUILD_TESTS=0]
 
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", *args
     system "make install" # if this fails, try separate make/make install steps
   end
 
