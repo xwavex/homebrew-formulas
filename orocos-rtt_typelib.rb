aa6ff84f166b7eeb26f4df379d578b1e51eebc31
Pouya <other.pouya@gmail.com>
2016-06-01 17:20:24 +0200

    rtt-typelib fromula

---
+++ orocos-rtt_typelib.rb
@@ -0,0 +1,13 @@
+class OrocosRtt_typelib < Formula
+  homepage "http://www.orocos.org"
+  head "https://github.com/orocos-toolchain/rtt_typelib.git"
+
+  depends_on "orocos-typelib"
+  depends_on "cmake"   => :build
+
+  def install
+  	args = std_cmake_args
+    system "cmake", ".", *args
+    system "make install"
+  end
+end
