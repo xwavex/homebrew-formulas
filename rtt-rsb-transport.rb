5b1a7d81d1c832e0a3b1963dbf2f813af9749330
Pouya <other.pouya@gmail.com>
2016-06-02 14:27:11 +0200

    inital commit

---
+++ rtt-rsb-transport.rb
@@ -0,0 +1,17 @@
+class RttRsbTransport < Formula
+  homepage "https://toolkit.cit-ec.uni-bielefeld.de"
+  head "https://github.com/corlab/rtt-rsb-transport.git"
+  
+  depends_on "rst-rt"
+  # depends_on "rst-proto"
+  depends_on "cmake"   => :build
+
+  def install
+    args = std_cmake_args+ %W[
+      -DCMAKE_MODULE_PATH=/usr/local/share/cmake/Modules
+    ]
+  	args = std_cmake_args
+    system "cmake", ".", *args
+    system "make install"
+  end
+end
