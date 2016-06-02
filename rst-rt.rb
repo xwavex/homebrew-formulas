c8c32b5c79f92c6b0bde3087b740c2e7e0e119fd
Pouya <other.pouya@gmail.com>
2016-06-02 14:28:36 +0200

    adding dependenceis

--- rst-rt.rb
+++ rst-rt.rb
@@ -2,7 +2,7 @@ class RstRt < Formula
   homepage "https://toolkit.cit-ec.uni-bielefeld.de"
   head "https://github.com/corlab/rst-rt.git"
 
-  depends_on "eigen"   => "2.91.0"
+  depends_on "eigen"
   depends_on "cmake"   => :build
 
   # because cmake might fail to locate eigen3 modules:
--- rtt-rsb-transport.rb
+++ rtt-rsb-transport.rb
@@ -3,7 +3,7 @@ class RttRsbTransport < Formula
   head "https://github.com/corlab/rtt-rsb-transport.git"
   
   depends_on "rst-rt"
-  # depends_on "rst-proto"
+  depends_on "rst-proto"
   depends_on "cmake"   => :build
 
   def install
