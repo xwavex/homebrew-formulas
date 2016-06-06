2b11baa2dc5ff5f5aace701ee3b9d6b968c0e467
xwavex <xwavedw@gmail.com>
2016-06-06 19:29:12 +0200

    added rst-rt typekit formula

---
+++ rtt-rst-rt-typekit.rb
@@ -0,0 +1,17 @@
+class RttRstRtTypekit < Formula
+  homepage "https://github.com/corlab/rtt-rst-rt-typekit"
+  url "https://github.com/corlab/rtt-rst-rt-typekit.git"
+
+  depends_on "eigen"
+  depends_on "rst-rt"
+  depends_on "orocos-rtt"
+  depends_on "cmake"   => :build
+
+  def install
+    args = std_cmake_args+ %W[
+      -DCMAKE_MODULE_PATH=#{HOMEBREW_PREFIX}/share/cmake/Modules
+    ]
+    system "cmake", ".", *args
+    system "make install"
+  end
+end
