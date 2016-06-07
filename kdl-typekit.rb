64f45ee2416ac5cb39ef7d79f340ed010bb5ade4
xwavex <xwavedw@gmail.com>
2016-06-07 23:45:10 +0200

    added kdl-typekit formula

--- kdl-typekit.rb
+++ kdl-typekit.rb
@@ -1,18 +1,20 @@
-require 'formula'
-
 class KdlTypekit < Formula
-  homepage 'https://github.com/corlab/rtt_geometry'
-  url 'https://github.com/corlab/rtt_geometry.git', :using => :git, :branch => 'indigo-devel'
-  head 'https://github.com/corlab/rtt_geometry.git', :using => :git
+  homepage 'https://github.com/orocos/rtt_geometry'
+  url 'https://github.com/orocos/rtt_geometry.git', :using => :git, :branch => 'indigo-devel'
+  head 'https://github.com/orocos/rtt_geometry.git', :using => :git
 
-  depends_on 'corlab/formulas/orocos-kdl'
-  depends_on 'orocos-rtt'
-  depends_on 'cmake' => :build
+  depends_on "orocos-kdl"
+  depends_on "orocos-rtt"
+  depends_on "cmake"   => :build
 
   def install
     args = std_cmake_args+ %W[
       -DCMAKE_MODULE_PATH=#{HOMEBREW_PREFIX}/share/cmake/Modules
     ]
+    # this is a hack until we have a proper solution.
+    #inreplace "./kdl_typekit/CMakeLists.txt" do |s|
+    #    s.gsub! "find_package(cmake_modules REQUIRED)", ""
+    #end
     system "cmake", "./kdl_typekit", *args
     system "make install"
   end
