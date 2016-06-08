8798cce60b95315a648cb259f38eb5a6b4f00a56
xwavex <xwavedw@gmail.com>
2016-06-08 23:31:49 +0200

    let kdl-typekit point to our fork of rtt_geometry

--- kdl-typekit.rb
+++ kdl-typekit.rb
@@ -1,9 +1,9 @@
 class KdlTypekit < Formula
-  homepage 'https://github.com/orocos/rtt_geometry'
-  url 'https://github.com/orocos/rtt_geometry.git', :using => :git, :branch => 'indigo-devel'
-  head 'https://github.com/orocos/rtt_geometry.git', :using => :git
+  homepage 'https://github.com/corlab/rtt_geometry'
+  url 'https://github.com/corlab/rtt_geometry.git', :using => :git, :branch => 'indigo-devel'
+  head 'https://github.com/corlab/rtt_geometry.git', :using => :git
 
-  depends_on "orocos-kdl"
+  depends_on "corlab/formulas/orocos-kdl"
   depends_on "orocos-rtt"
   depends_on "cmake"   => :build
 
@@ -11,10 +11,6 @@ class KdlTypekit < Formula
     args = std_cmake_args+ %W[
       -DCMAKE_MODULE_PATH=#{HOMEBREW_PREFIX}/share/cmake/Modules
     ]
-    # this is a hack until we have a proper solution.
-    #inreplace "./kdl_typekit/CMakeLists.txt" do |s|
-    #    s.gsub! "find_package(cmake_modules REQUIRED)", ""
-    #end
     system "cmake", "./kdl_typekit", *args
     system "make install"
   end
