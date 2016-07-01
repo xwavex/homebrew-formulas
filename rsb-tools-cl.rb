fe2fa4e4db6005ac5c0a315a088b089b87004c3d
xwavex <xwavedw@gmail.com>
2016-07-01 11:02:48 +0200

    fixed checksum

--- rsb-tools-cl.rb
+++ rsb-tools-cl.rb
@@ -3,7 +3,7 @@ require 'formula'
 class RsbToolsCl < Formula
   homepage 'https://code.cor-lab.org/projects/rsb'
   url 'https://ci.cor-lab.org/job/rsb-tools-cl-0.13-macos/label=MAC_OS_mavericks_64bit/lastSuccessfulBuild/artifact/build/rsb', :using => :nounzip
-  sha256 '002c8d6b4e7f8610d46eb2f62443233e8f91b141ba96e7a2991e8fa9f34f4282'
+  sha256 '84c594ae44af97620a1d636e4f68cb13e579c9d5fcd7da9637ae024440a15f54'
   version '0.13'
 
   def install
